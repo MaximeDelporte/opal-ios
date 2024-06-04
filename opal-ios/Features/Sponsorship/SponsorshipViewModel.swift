4b223f8b7bd0090835814d9ca7906a8003c9233c
Maxime Delporte <62073284+MaximeDelporte@users.noreply.github.com>
2024-06-04 17:28:28 +0200

    Passing loadRewards test.

---
+++ opal-ios/Features/Sponsorship/SponsorshipViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  SponsorshipViewModel.swift
+//  opal-ios
+//
+//  Created by afx on 04/06/2024.
+//
+
+import Foundation
+
+class SponsorshipViewModel {
+    
+    func loadRewards() {
+        
+    }
+}
