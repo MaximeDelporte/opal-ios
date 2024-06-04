27ac77b532d885aacab68e6b3d485282963b2677
Maxime Delporte <62073284+MaximeDelporte@users.noreply.github.com>
2024-06-04 17:02:00 +0200

    Creating Reward struct. Passing test: test_reward_properties.

---
+++ opal-ios/Models/Reward.swift
@@ -0,0 +1,17 @@
+//
+//  Reward.swift
+//  opal-ios
+//
+//  Created by afx on 04/06/2024.
+//
+
+import Foundation
+
+struct Reward {
+    
+    let imageUrl: String
+    let requiredFriends: Int
+    let title: String
+    let description: String
+    let isPremiumReward: Bool?
+}
