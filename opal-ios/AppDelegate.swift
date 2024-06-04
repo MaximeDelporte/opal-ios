a175cf4ec2906f3500774d99182599438fbb66f6
Maxime Delporte <62073284+MaximeDelporte@users.noreply.github.com>
2024-06-04 16:48:08 +0200

    Adding rewards.json file that will be used for the tests. Setup HomeViewController with a button to redirect to ReferredViewController.

--- opal-ios/AppDelegate.swift
+++ opal-ios/AppDelegate.swift
@@ -13,10 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let homeController = HomeViewController()
+        let navigationController = UINavigationController(rootViewController: homeController)
+        
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
+        
         return true
     }
 }
-
