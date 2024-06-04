68b2c3f21b5a994a10010288271afd83609d9382
Maxime Delporte <62073284+MaximeDelporte@users.noreply.github.com>
2024-06-04 16:52:33 +0200

    Creating SponsorshipViewController.

---
+++ opal-ios/Features/Sponsorship/SponsorshipViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ReferredViewController.swift
+//  opal-ios
+//
+//  Created by afx on 04/06/2024.
+//
+
+import Foundation
+import UIKit
+
+class SponsorshipViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
